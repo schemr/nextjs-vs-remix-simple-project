467c3167a9a8013bff0f97cfebc9d62456ef54de
Seul ki, Lee <schemryj@gmail.com>
2022-02-04 16:44:09 +0900

    Add index page in remix

--- my-library-remix/app/routes/index.tsx
+++ my-library-remix/app/routes/index.tsx
@@ -1,32 +1,23 @@
+import { Link } from "remix";
+
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <div className="container mx-auto flex flex-col items-center py-12 sm:py-24">
+          <div className="w-11/12 sm:w-2/3 lg:flex justify-center items-center flex-col  mb-5 sm:mb-10">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl text-center text-gray-800 font-black leading-7 md:leading-10">
+              My
+              <span className="text-indigo-700 pl-3">
+                <Link to="/books">Library</Link>
+              </span>
+            </h1>
+            <p className="mt-5 sm:mt-10 lg:w-10/12 text-gray-400 font-normal text-center text-sm sm:text-lg">
+              Notion Database Table
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
